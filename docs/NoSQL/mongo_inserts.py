6ddbc4fd73ed4d570008da6a1ed72f08ccb11f44
entangelk <kdtyohan@gmail.com>
2023-12-13 17:52:08 +0900

    go home

--- docs/NoSQL/mongo_inserts.py
+++ docs/NoSQL/mongo_inserts.py
@@ -1,6 +1,21 @@
-import <stdio.h>
+from pymongo import MongoClient
+# connect mongodb -> 접속 자원에 대한 class 입력
+mongoclient = MongoClient('mongodb://localhost:27017')
+# database 연결
+db_local = mongoclient["local"]
+# collection 작업
+collection = db_local['fruits']
+# insert 작업
+collection.insert_one({
+    'name': 'lemon',
+    'color': 'yellow',
+    'origin': 'Spain'
+})
+dict_fruit = {
+    'name': 'orange',
+    'color': 'orange',
+    'origin': 'Spain'
+    }
 
-int main (viod){
-    printf("hello world");
-    return 0;
-}
+collection.insert_one(dict_fruit)
+pass
