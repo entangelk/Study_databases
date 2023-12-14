cbff8d8e1c0ad1b55abeb2143baa46a81aacc6fb
entangelk <kdtyohan@gmail.com>
2023-12-14 10:07:24 +0900

    update

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -48,26 +48,26 @@ fruits_info = [
     }
 ]
 
-class mongo_local_insert:
+class mongo_local_insert:   # 로컬 데이터베이스 인서트 클래스 선언
     def __init__(self,collection_name) -> None:
-        self.address='mongodb://localhost:27017'
-        self.host="local"
+        self.address='mongodb://localhost:27017'    # 호스트 ip 어드레스 지정
+        self.host="local"   # 로컬 타입
         self.insert_collection=''
-        self.collection_name = collection_name
+        self.collection_name = collection_name  # 콜렉션 변수 저장
         pass
 
     def connect(self):
-        mongoclient = MongoClient(self.address)
-        db_local = mongoclient[self.host]
-        self.insert_collection = db_local[self.collection_name]
-        return self.insert_collection
+        mongoclient = MongoClient(self.address) #mongo접속
+        db_local = mongoclient[self.host]   #database 연결
+        self.insert_collection = db_local[self.collection_name] #collection 진입
+        return self.insert_collection   #진입 결과 리턴
     
     def insert(self,collection):
-        for i in range(len(collection)):
+        for i in range(len(collection)):    #insert 대신 insertOne으로 길이만큼 주소 반복
             self.insert_collection.insert_one(collection[i])
             pass
         return
     
-loinsert=mongo_local_insert('fruits')
-loinsert.connect()
-loinsert.insert(fruits_info)
+loinsert=mongo_local_insert('fruits') #collection 입력에 대한 클래스 인스턴스
+loinsert.connect()  #DB 연결 함수 호출
+loinsert.insert(fruits_info)    #입력된 collection에 대한 inforlist insert
