bd769cd7cbb09e8dd08db8fbdcf797f566af2b2b
entangelk <kdtyohan@gmail.com>
2023-12-15 10:19:10 +0900

    update

--- docs/quests/solvingProblem_functions.py
+++ docs/quests/solvingProblem_functions.py
@@ -17,21 +17,21 @@ class mongo_get_question:
         self.insert_collection = db_local[self.collection_name] #collection 진입
         return self.insert_collection   #진입 결과 리턴
     
-    def struct_save(self):
+    def struct_save(self):  # DB에서 콜렉션 출력 후 리스트 저장
         self.total_db = list(self.insert_collection.find({},{"_id":1, "question":1, "choices":1, "answer":1,"score":1}))
         return self.total_db
 
     def pirnt_and_input(self):
         for i in range(len(self.total_db)):
-            print("문제 {} : {}".format(i+1,self.total_db[i]['question']))
+            print("문제 {} : {}".format(i+1,self.total_db[i]['question']))  # 퀘스쳔 부분 출력
             for j in range(4):
-                print("{}. {}".format(j+1,self.total_db[i]['choices'][j]))
+                print("{}. {}".format(j+1,self.total_db[i]['choices'][j]))  # 초이스 부분 출력
                 pass
-            get_num = int(input('답을 입력하세요 : '))
+            get_num = int(input('답을 입력하세요 : '))  # 유저 입력값 저장
             self.get_answer.append(get_num)
-            if self.total_db[i]['choices'][self.get_answer[i]-1] == self.total_db[i]['answer']:
+            if self.total_db[i]['choices'][self.get_answer[i]-1] == self.total_db[i]['answer']: #정답과 비교
                 print('정답입니다!')
-                self.result_score += self.total_db[i]['score']
+                self.result_score += self.total_db[i]['score']  # 정답 시 스코어값 호출 후 저장
                 pass
             else:
                 print('오답입니다!')
@@ -40,16 +40,14 @@ class mongo_get_question:
         return
     
     def result_answer(self):
-        print("최종점수 : {}".format(self.result_score))
+        print("최종점수 : {}".format(self.result_score))    # 스코어값 저장된 총합 출력
         return
     
-    def insert_db(self):
+    def insert_db(self):    # 유저 입력값 DB 업로드
         for i, doc in enumerate(self.total_db):
             self.insert_collection.update_many({'_id': doc['_id']},{'$set': {'User_answer': self.get_answer[i]}})
             pass
         return
-        # for i in range(len(self.total_db)):
-        #     self.insert_collection.update_many({'category' : self.total_db[i]['_id']}, { $set : {'User_answer' : get_answer[i]}})
 
 
 
--- docs/quests/solvingProblem_main.py
+++ docs/quests/solvingProblem_main.py
@@ -2,8 +2,8 @@ from solvingProblem_functions import mongo_get_question
 
 
 testquestion=mongo_get_question('mongodb://localhost:27017','local','solvingProblem') # address, database, collection 입력에 대한 클래스 인스턴스
-testquestion.connect()
-testquestion.struct_save()
-testquestion.pirnt_and_input()
-testquestion.result_answer()
-testquestion.insert_db()
+testquestion.connect()  # 몽고 연결
+testquestion.struct_save()  # 데이터 베이스에서 받아온 리스트 저장 및 가공
+testquestion.pirnt_and_input()   # 프린트 및 인풋
+testquestion.result_answer()    # 전체 결과 체크 및 결과 출력
+testquestion.insert_db()    # 입력결과 DB 업데이트
