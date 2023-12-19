1fcf57db40a88df011fa10d2bca30b1e5e68715b
entangelk <kdtyohan@gmail.com>
2023-12-19 18:44:12 +0900

    update

---
+++ docs/temp.py
@@ -0,0 +1,98 @@
+[
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b314'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b30f'),
+        'Answers': 2,
+
+    },
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b314'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b310'),
+        'Answers': 4,
+
+    },
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b314'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b311'),
+        'Answers': 1,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b314'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b312'),
+        'Answers': 3,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b314'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b313'),
+        'Answers': 2,
+
+    },
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b315'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b30f'),
+        'Answers': 2,
+
+    },
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b315'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b310'),
+        'Answers': 4,
+
+    },
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b315'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b311'),
+        'Answers': 1,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b315'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b312'),
+        'Answers': 3,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b315'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b313'),
+        'Answers': 2,
+
+    },
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b316'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b30f'),
+        'Answers': 2,
+
+    },
+    {
+        'User_id': ObjectId('65813f779cc3b6e925d3b316'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b310'),
+        'Answers': 4,
+
+    },
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b316'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b311'),
+        'Answers': 1,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b316'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b312'),
+        'Answers': 3,
+
+    },
+    
+        {
+        'User_id': ObjectId('65813f779cc3b6e925d3b316'),
+        'Question_id': ObjectId('65813cee9cc3b6e925d3b313'),
+        'Answers': 2,
+
+    }
+]
